681094c7aacf22afa2aec8ebcae865770e98add6
mhadziq0403 <mhadziq0403@gmail.com>
2022-09-07 18:23:17 +0700

    Tidak jadi coba deploy hehe

--- project_django/settings.py
+++ project_django/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'wishlist.urls'
+ROOT_URLCONF = 'project_django.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'wishlist.wsgi.application'
+WSGI_APPLICATION = 'project_django.wsgi.application'
 
 
 # Database
