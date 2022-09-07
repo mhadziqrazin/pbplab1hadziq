d38ff5923ac3a21517f8985f966bf066f0a9ce71
mhadziqrazin <88391977+mhadziqrazin@users.noreply.github.com>
2022-09-07 18:11:08 +0700

    Update settings.py

--- project_django/settings.py
+++ project_django/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'project_django.urls'
+ROOT_URLCONF = 'wishlist.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'project_django.wsgi.application'
+WSGI_APPLICATION = 'wishlist.wsgi.application'
 
 
 # Database
